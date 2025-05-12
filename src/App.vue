bd6e840f9fcb27b927d14f796ae660ef707039ee
Aifrom <yxlform@163.com>
2025-05-12 19:41:00 +0800

    第一版本

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div id="app">
+        <h1>Welcome to Your Vue.js App</h1>
+        <p>To get started, edit <code>src/App.vue</code> and save to reload.</p>
+        <p>For more information, visit the <a href="https://vuejs.org">Vue.js documentation</a>.</p>
+    </div>
+  
+</template>
+
+<style scoped>
+
+</style>
